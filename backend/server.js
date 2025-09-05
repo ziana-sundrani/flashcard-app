585199f59bc7ac55e1e6c846226ba78f6094ded9
ziana_sundrani <zianasundrani@gmail.com>
2025-09-05 18:09:05 -0400

    fix backend deployment bug

--- backend/server.js
+++ backend/server.js
@@ -226,4 +226,4 @@ app.post('/api/generate-flashcards', async (req, res) => {
 
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
